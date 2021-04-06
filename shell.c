5e4aa97c353c4ff6793a6d79c0d335efca7de14b
逆瀬川愛貴子 <sakasekawameiko@172-1-103-30.lightspeed.brhmal.sbcglobal.net>
2021-04-06 16:14:36 +0900

    readline_done

--- shell.c
+++ shell.c
@@ -4,9 +4,11 @@
 #include <readline/readline.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "libft/libft.h"
 
 int	cd(char *path) {
 	return chdir(path);//OK
+	//相対パスは今後対応
 }
 
 char **get_input(char *input) {
@@ -31,7 +33,7 @@ char **get_input(char *input) {
 	return command;
 }
 
-int	main(int argc, char **argv)
+int	main()
 {
 	char **command;
 	char *input;
@@ -40,15 +42,18 @@ int	main(int argc, char **argv)
 
 	//if (argc != 1)
 	//エラー処理
-	while (1) {
-		printf("unixsh> ");
+	while (1)
+	{
+		ft_putstr_fd("unixsh> ", 0);
 		//エラー処理追記必要あり
 		get_next_line(0, &input);
 		//input = readline("unixsh> ");//NG:printfで>を出力して、あとはgetnextlineの標準入力バージョンで文字列を出力
 		command = get_input(input);//いったんそっとしておくけど二次元配列に格納
 		
-		if (strcmp(command[0], "cd") == 0) {
-			if (cd(command[1]) < 0) {
+		if (strcmp(command[0], "cd") == 0)
+		{
+			if (cd(command[1]) < 0)
+			{
 				perror(command[1]);//NG:strerrorかerroroに変える：最後に処理で良い
 			}
 		}
@@ -58,14 +63,18 @@ int	main(int argc, char **argv)
 			perror("Fork failed");
 			exit(1);
 		}
-		if (child_pid == 0) {//folkに成功した場合
+		if (child_pid == 0)
+		{//folkに成功した場合
 			if (execvp(command[0], command) < 0)//NG:コマンド読んでくれる関数execveは使える
 			{
 				perror(command[0]);//NG
 				exit(1);
 			}
 			printf("no message if execvp success");
-		} else {
+		}
+		else 
+		{
+			//while (!WIFEXITED(stat_loc) && !WIFSIGNALED(stat_loc))
 			waitpid(child_pid, &stat_loc, WUNTRACED);
 			//OK:子プロセスが終わるまで親プロセスを停止
 			//https://www.delftstack.com/ja/howto/c/waitpid-in-c/
@@ -73,6 +82,9 @@ int	main(int argc, char **argv)
 			//WUNTRACED:終了だけでなく、他に停止した子プロセスについて報告。
 			//errnoここで使うかも
 			//https://www.ibm.com/docs/ja/zos/2.3.0?topic=functions-waitpid-wait-specific-child-process-end
+		}
+		free(input);
+		free(command);
+	}
 	return (0);
-
 }
