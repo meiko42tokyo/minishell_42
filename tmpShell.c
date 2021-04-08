7de117fa67d0545cf4c8457ec58dcd006dbef531
upon0426 <karasawa.yui@gmail.com>
2021-04-08 17:56:18 +0900

    tmp: 3 nodes in linked list

--- tmpShell.c
+++ tmpShell.c
@@ -2,12 +2,17 @@
 
 int	main() {
 	// test data
-	char *content = "ls -l";
+	char *content1 = "echo aaa";
+	char *content2 = "sleep 10";
+	char *content3 = "echo bbb";
 	t_cmd *head;	
-	t_cmd *new;
-	head = ft_cmdnew(content, 124);
-	new = ft_cmdnew(content, 124);
-	ft_cmdadd_back(&head, new);
+	t_cmd *new1;
+	t_cmd *new2;
+	head = ft_cmdnew(content1, 124);
+	new1 = ft_cmdnew(content2, 124);
+	new2 = ft_cmdnew(content3, 0);
+	ft_cmdadd_back(&head, new1);
+	ft_cmdadd_back(&head, new2);
 	
 	// debug
 	while (head)
