19e756a5604f57d7320adfed25caa1fe0a704045
upon0426 <karasawa.yui@gmail.com>
2021-04-08 18:29:46 +0900

    tmp: made run_list and do_pipeline

--- tmpShell.c
+++ tmpShell.c
@@ -1,5 +1,59 @@
 #include "shell.h"
 
+int	isbuiltin(t_cmd *c)
+{
+	// Temporary always return false.
+	// TODO: Should be judged by type of command
+	if (c->content)
+		return (0);
+	return (0);
+}
+
+int	ispipe(t_cmd *c)
+{
+	if (c->op == 124)
+		return (1);
+	return (0);
+}
+
+t_cmd	*do_pipeline(t_cmd *c)
+{
+	int	haspipe;
+	int	lastpipe[2];
+
+	haspipe = 0;
+	lastpipe[0] = -1;
+	lastpipe[1] = -1;
+	while (c)
+	{
+		//c->pid = start_command(c, ispipe(c), haspipe, lastpipe);
+		haspipe = ispipe(c);
+		if (haspipe)
+			c = c->next;
+		else
+			break;
+	}
+	return (c);
+}
+
+
+void	run_list(t_cmd *c)
+{
+	while (c)
+	{
+		if (isbuiltin(c) && !ispipe(c))
+		{
+			//execve(c);
+			c = c->next;
+			continue;
+		}
+		c = do_pipeline(c);
+		//waitpid(c->pid);
+		printf("node:%s, %c\n", c->content, c->op); 
+		c = c->next;
+	}
+}
+
 int	main() {
 	// test data
 	char *content1 = "echo aaa";
@@ -14,12 +68,15 @@ int	main() {
 	ft_cmdadd_back(&head, new1);
 	ft_cmdadd_back(&head, new2);
 	
+	run_list(head);
+
 	// debug
+	/*
 	while (head)
 	{
-		printf("head:%s, %c\n", head->content, head->op); 
 		if (head->next == NULL)
 			break;
 		head=head->next;
 	}
+	*/
 }
