afbe5eb466e738e1ce6d0e41c7ae4932f0457a48
upon0426 <karasawa.yui@gmail.com>
2021-06-01 16:55:01 +0900

    for tester -c

--- shell.c
+++ shell.c
@@ -6,18 +6,23 @@ int	main(int argc, char **argv)
 	t_cmd		*head;
 	t_env	*env;
 	
-	argc = 1;
-	argv = NULL;
 	line = NULL;
 	head = NULL;
 	signal(SIGINT, SIG_IGN);
 	env = init_env();
 	while (1) {
 		ft_putstr_fd("> ", 0);
-		get_next_line(0, &line);
-		head = make_cmdlist(line);
-		free(line);
-		line = NULL;
+		if (argc > 2 && ft_strncmp("-c", argv[1], 3) == 0)
+		{
+			head = make_cmdlist(argv[2]);
+		}
+		else
+		{
+			get_next_line(0, &line);
+			head = make_cmdlist(line);
+			free(line);
+			line = NULL;
+		}
 		signal(SIGINT, SIG_DFL);
 		if (head != NULL)
 			run_list(head, env);
