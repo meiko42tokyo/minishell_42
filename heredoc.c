14388d655e077adecb00a45b7c6e860c733a86db
upon0426 <karasawa.yui@gmail.com>
2021-09-04 14:30:17 +0900

    get identifier

--- heredoc.c
+++ heredoc.c
@@ -16,8 +16,29 @@ int	heredoc_exist(char *input, int *times)
 	return (*times);
 }
 
+char	*get_identifier(char *input)
+{
+	int	len;
+
+	input += 2;
+	while (ft_isspace(*input))
+		input++;
+	len = ft_strlen(input);
+	if (ft_strchr(input, ' '))
+		len = ft_strchr(input, ' ') - input;
+	return (ft_strndup(input, len));
+}
+
 int	heredoc(char *input)
 {
+	// get identifier
+	// expand identifier
+	char	*identifier;
+
+	identifier = get_identifier(input);
+	// store input while line != identifier
+	// connect saved str to standard input
+	// remove << and aaa from argv
 	printf("heredoc:%s\n", input);
 	return (0);
 }
