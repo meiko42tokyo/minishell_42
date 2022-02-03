c1e992b6ec79c9e6c4a7309fbbc4befb477fbcd8
upon0426 <karasawa.yui@gmail.com>
2022-02-03 19:06:03 +0900

    tabs

--- close_utility.c
+++ close_utility.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
-void    multi_close(int *fd1, int *fd2, int *fd3, int *fd4)
+void	multi_close(int *fd1, int *fd2, int *fd3, int *fd4)
 {
-    if (fd1)
-        close(*fd1);
-    if (fd2)
-        close(*fd2);
-    if (fd3)
-        close(*fd3);
-    if (fd4)
-        close(*fd4);
+	if (fd1)
+		close(*fd1);
+	if (fd2)
+		close(*fd2);
+	if (fd3)
+		close(*fd3);
+	if (fd4)
+		close(*fd4);
 }
